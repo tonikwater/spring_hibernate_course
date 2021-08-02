ee60b698bdd5350e7f8445a35085b802d834ddbc
tonikwater <martinrehbock@hotmail.de>
2021-08-02 22:47:04 +0200

    sec 70-73: spring boot intro

---
+++ 05-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${coach.team}")
+	private String teamName;
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello, World! Time on server is " + LocalDateTime.now();
+	}
+	
+	@GetMapping("/workout")
+	public String getDailyWorkout() {
+		return "Run a hard 5k!";
+	}
+	
+	@GetMapping("/fortune")
+	public String getDailyFortune() {
+		return "Today is your lucky day.";
+	}
+	
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + " Team: " + teamName;
+	}
+}
